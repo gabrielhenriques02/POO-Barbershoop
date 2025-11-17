8cf25fbca3e580ee53f59e357ec85dd2ef71075f
caiofaraujo-beep <caio.f.araujo@edu.ufes.br>
2025-11-17 11:36:07 -0300

    Create administrador.py

---
+++ administrador.py
@@ -0,0 +1,18 @@
+from pessoa import Pessoa
+
+class Administrador:
+    def __init__(self, id, nome, celular, email, ativo, nivel_acesso, criado_em):
+
+        super().__init__(id, nome, celular, email)
+
+        self.ativo = ativo
+        self.nivel_acesso = nivel_acesso
+        self.criado_em = criado_em
+
+    def cadastro_profissionais(self):
+
+        print() #editar
+    
+    def gerenciar_servico(self):
+
+        print() #editar
